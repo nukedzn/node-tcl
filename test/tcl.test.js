385f9d903a9ae817ea71e07af82a2fc931793929
Uditha Atukorala <udi@geniusse.com>
2015-04-24 23:05:18 +0100

    updated tests

--- test/tcl.test.js
+++ test/tcl.test.js
@@ -1,19 +1,39 @@
+/* jshint expr: true */
 
 'use strict';
 
-var Tcl = require( '../' );
+var expect = require( 'chai' ).expect;
+var Tcl    = require( '../' );
+
 
 describe( 'tcl', function () {
 
-	it( 'should be able to return an object', function ( done ) {
+	it( 'should be able to create a new Tcl object', function () {
 		var tcl = new Tcl();
-		done();
+		expect( tcl ).to.have.property( '_interp' );
+		expect( tcl._interp ).to.be.an( 'object' );
 	} );
 
 
-	it( 'should be able to execute a tcl command', function ( done ) {
-		var tcl = new Tcl();
-		tcl.cmd( 'info commands', done );
+	context( 'when there is a valid Tcl object', function () {
+
+		// tcl object placeholder
+		var tcl = {};
+
+		beforeEach( function () {
+			tcl = new Tcl();
+		} );
+
+		it( 'should be able to get Tcl version', function ( done ) {
+			tcl.cmd( 'info tclversion', function ( err, data ) {
+				var version = parseFloat( data );
+
+				expect( err ).to.be.null;
+				expect( isNaN( version ) ).to.be.false;
+				expect( version ).to.be.a( 'number' );
+				done();
+			} );
+		} );
 	} );
 
 } );

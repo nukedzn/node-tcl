c562078a4fee2926ff2dd6a12c3393e50c46a53f
Uditha Atukorala <udi@geniusse.com>
2015-10-29 17:51:40 +0000

    take CXX env var into account when checking c++ capabilities

--- gyp/cxx.sh
+++ gyp/cxx.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cxx=`g++ -E -dM -std=c++11 -x c++ /dev/null 2>/dev/null | grep __cplusplus | awk '{print $3}'`;
+if [ -z "${CXX}" ] ; then
+	CXX=g++;
+fi
+
+cxx=`${CXX} -E -dM -std=c++11 -x c++ /dev/null 2>/dev/null | grep __cplusplus | awk '{print $3}'`;
 
 if [ "$cxx" == "201103L" ]; then
 	echo "$cxx";

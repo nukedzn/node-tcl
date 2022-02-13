048dd5103732a7ba1876317a38b4fc5dc8bcd86f
Uditha Atukorala <ua@nuked.zone>
2022-02-13 20:40:50 +0000

    issue #16 - fix lint

--- test/proc.js
+++ test/proc.js
@@ -8,7 +8,7 @@ var tcl    = require( '../' );
 describe( 'Call back to JS function', function () {
 
 	it( 'should be able to define a new Tcl command', function (done) {
-		var res = tcl.proc('abc', (...args) => {
+		tcl.proc('abc', (...args) => {
 			expect( args.length ).to.equal( 2 );
 			expect( args[0] ).to.equal( 'hi' );
 			expect( args[1] ).to.equal( 'there' );
@@ -18,15 +18,17 @@ describe( 'Call back to JS function', function () {
 	});
 
 	it( 'should be able to define distinct Tcl commands', function (done) {
-		var res = tcl.proc('qrs', (...args) => {
+		tcl.proc('qrs', (...args) => {
 			expect( args[0] ).to.equal( 'what' );
 			expect( args[1] ).to.equal( 'up' );
 		});
-		var res = tcl.proc('xyz', (...args) => {
+
+		tcl.proc('xyz', (...args) => {
 			expect( args[0] ).to.equal( 'ho' );
 			expect( args[1] ).to.equal( 'here' );
 			done();
 		});
+
 		tcl.cmdSync('qrs what up');
 		tcl.cmdSync('xyz ho here');
 	});
